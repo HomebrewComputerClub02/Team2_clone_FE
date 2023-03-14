492f9718faebf0c9b534f9e157b9db1a71e34cfc
BaeJ1H021 <blueapple99@yonsei.ac.kr>
2023-03-15 01:30:20 +0900

    FEAT : WebPlayer NavBar icon 추가

--- src/components/molecules/NavBar.tsx
+++ src/components/molecules/NavBar.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import LibraryAddIcon from '@mui/icons-material/LibraryAdd';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const Nav = styled.div`
   background-color: #000;
@@ -9,29 +14,48 @@ const Nav = styled.div`
   min-height: 0;
   position: relative;
   z-index: 4;
-  padding-top: 24px;
+  padding-top: 100px; //나중에 24로 수정.
   padding-left: 24px;
   padding-right: 24px;
 `;
 const NavUl = styled.ul`
   display: flex;
   flex-direction: column;
-  gap: 15px;
+  gap: 20px;
 `;
 const NavLi = styled.li`
   color: white;
+`;
+const NavA = styled.a`
+  display: flex;
+  gap: 16px;
+  align-items: center;
+`;
+const NavSpan = styled.span`
+  font-weight: 700;
+  font-size: 14px;
 `;
 const NavDiv = styled.div`
   display: flex;
   flex-direction: column;
   gap: 15px;
-  margin-top: 24px;
+  margin-top: 40px;
 `;
 const NavInnerDiv = styled.div`
   color: white;
 `;
+const NavButton = styled.button`
+  align-items: center;
+  display: flex;
+  gap: 16px;
+  background-color: transparent;
+  color: #fff;
+  border: none;
+  padding: 0px;
+`;
 const HrDiv = styled.div`
   margin-top: 10px;
+  margin-bottom: 10px;
   vertical-align: baseline;
 `;
 const Hr = styled.hr`
@@ -43,13 +67,38 @@ const NavBar = () => {
   return (
     <Nav>
       <NavUl>
-        <NavLi>홈</NavLi>
-        <NavLi>검색하기</NavLi>
-        <NavLi>내 라이브러리</NavLi>
+        <NavLi>
+          <NavA>
+            <HomeIcon />
+            <NavSpan>홈</NavSpan>
+          </NavA>
+        </NavLi>
+        <NavLi>
+          <NavA>
+            <SearchIcon />
+            <NavSpan>검색하기</NavSpan>
+          </NavA>
+        </NavLi>
+        <NavLi>
+          <NavA>
+            <LibraryMusicIcon />
+            <NavSpan>내 라이브러리</NavSpan>
+          </NavA>
+        </NavLi>
       </NavUl>
       <NavDiv>
-        <NavInnerDiv>플레이리스트 만들기</NavInnerDiv>
-        <NavInnerDiv>좋아요 표시한 곡</NavInnerDiv>
+        <NavInnerDiv>
+          <NavButton>
+            <LibraryAddIcon />
+            <NavSpan>플레이리스트 만들기</NavSpan>
+          </NavButton>
+        </NavInnerDiv>
+        <NavInnerDiv>
+          <NavA>
+            <FavoriteIcon />
+            <NavSpan>좋아요 표시한 곡</NavSpan>
+          </NavA>
+        </NavInnerDiv>
       </NavDiv>
       <HrDiv>
         <Hr />
