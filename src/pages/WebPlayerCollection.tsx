9b67cdf8d4744efc2aff1dd5f2dd6ce04d557206
BaeJ1H021 <blueapple99@yonsei.ac.kr>
2023-03-27 14:15:35 +0900

    FEAT : 내 라이브러리 클릭시 나와야 하는 Collection Page 추가 (Outlet 렌더링하는 상위 페이지)
    클릭시 collection/playlists로 이동.

---
+++ src/pages/WebPlayerCollection.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import MainViewFooter from '../components/molecules/MainViewFooter';
+import Section from '../components/molecules/Section';
+import { Outlet } from 'react-router-dom';
+
+const MainView = styled.div`
+  background-color: #121212;
+  display: flex;
+  flex-direction: column;
+  grid-area: main-view;
+  min-height: 0;
+  overflow: hidden;
+  position: relative;
+  width: 100%;
+  min-width: 520px;
+  padding-top: 24px;
+  padding-left: 24px;
+  padding-right: 24px;
+  padding-bottom: 32px;
+  box-sizing: border-box; // box크기에 padding영역과 border영역을 포함해준다.
+  gap: 24px;
+  overflow-y: auto;
+  &::-webkit-scrollbar {
+    width: 15px;
+    background: rgba(233, 7, 53, 0.4); // 차이 두기 위해 색깔 추가
+  }
+  &::-webkit-scrollbar-thumb {
+    height: 10%; // 알아서 자동 설정이라 사실 무의미.
+    background: #217af4;
+  }
+`;
+
+const WebPlayerCollection = () => {
+  return (
+    <MainView>
+      <Outlet />
+      <MainViewFooter />
+    </MainView>
+  );
+};
+
+export default WebPlayerCollection;
