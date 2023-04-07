337c8ad8289eb20402458b8a636ab7ee4756d52a
Hongjin <wad30905@gmail.com>
2023-04-07 21:12:31 +0900

    like api

--- src/components/molecules/MusicProfile.tsx
+++ src/components/molecules/MusicProfile.tsx
@@ -6,6 +6,8 @@ import { MdOutlineFilterFrames } from 'react-icons/md';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import { BASE_URL, musicId } from '../../stores/SampleData';
+import axios from 'axios';
 export interface MusicProfileProps {
   ImgSrc: string;
   artist: string;
@@ -85,10 +87,31 @@ function MusicProfile({ ImgSrc, artist, song }: MusicProfileProps) {
     //좋아요 조회 api
     setIsLiked(true);
   }, []);
+  const likeSong = async () => {
+    console.log(localStorage.getItem('jwtToken'));
+    axios
+      .post(`${BASE_URL}/like/${musicId}`, {
+        headers: {
+          Authorization: localStorage.getItem('jwtToken'),
+          'Access-Control-Allow-Origin': `http://172.30.1.48:3000`,
+          'Access-Control-Allow-Credentials': 'true',
+        },
+        withCredentials: true,
+      })
+      .then((res: any) => {
+        console.log('successfully liked');
+      })
+      .catch((e) => {
+        console.log(e);
+        console.log(e.message);
+        console.log(e.code);
+      });
+  };
   const toggleLike = () => {
     if (!isLiked) {
       setIsLiked(true);
       //좋아요 api보냄
+      likeSong();
     } else {
       setIsLiked(false);
     }
--- src/stores/AuthService.ts
+++ src/stores/AuthService.ts
@@ -30,6 +30,7 @@ export interface LoginProps {
 class AuthService {
   private BASE_URL = `http://172.30.1.43:8080`;
   private TOKEN_KEY = 'jwtToken';
+  private REFRESH_TOKEN_KEY = 'refreshToken';
   public cookie = new Cookies();
   public async signup({
     email,
@@ -70,9 +71,9 @@ class AuthService {
         const refreshJwt = res.headers.get('Refresh');
         console.log('By Login Access:' + accessJwt);
         console.log('By Login Refresh:' + refreshJwt);
-        localStorage.setItem('token', accessJwt);
+        localStorage.setItem(this.TOKEN_KEY, accessJwt);
         //도메인 달라지면 쿠키 사용 불가능하니 일단 그냥 로컬스토리지에도 저장
-        localStorage.setItem('refresh', refreshJwt);
+        localStorage.setItem(this.REFRESH_TOKEN_KEY, refreshJwt);
       });
   }
 
