3af9f8c494236b9b99255a33a021390aa03b8315
BaeJ1H021 <blueapple99@yonsei.ac.kr>
2023-04-28 13:20:00 +0900

    FEAT : 좋아요 표시한 곡 api 작업

---
+++ src/components/molecules/LikeBottomDiv.tsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import { Track } from '../../stores/SampleData';
+import Song from './Song';
+import { useRecoilState } from 'recoil';
+import { GlobalTracks } from '../../stores/atom';
+
+interface Music {
+  trackId: string;
+  title: string;
+  seconds: number;
+  singerList: Array<Singer>;
+}
+
+interface Singer {
+  singerId: string;
+  singerName: string;
+  imgUrl: string;
+}
+
+interface Props {
+  data: Array<Music>;
+}
+
+export const BottomDiv = styled.div`
+  border: 1px solid transparent;
+  border-radius: 4px;
+  padding: 0 32px;
+`;
+export const OutlineGrid = styled.div`
+  display: grid;
+  grid-template-columns:
+    [index] 16px [title] 6fr [album] 4fr
+    [date] 3fr [time] minmax(120px, 1fr);
+  @media screen and (max-width: 970px) {
+    grid-template-columns:
+      [index] 16px [title] 6fr [album] 4fr
+      [time] minmax(120px, 1fr);
+  }
+  @media screen and (max-width: 770px) {
+    grid-template-columns:
+      [index] 16px [title] 6fr
+      [time] minmax(120px, 1fr);
+  }
+  border-bottom: 1px solid hsla(0, 0%, 100%, 0.1);
+  grid-gap: 16px;
+  color: #b3b3b3;
+  height: 36px;
+  align-items: center;
+  padding: 0 16px;
+`;
+
+export const OutlineSpan = styled.span`
+  font-size: 16px;
+  font-weight: 400;
+`;
+
+export const FlexDiv = styled.div`
+  margin-top: 16px;
+  display: flex;
+  flex-direction: column;
+`;
+export interface props {
+  data: Track[];
+}
+
+const LikeBottomDiv = ({ data }: props) => {
+  const [globalTracks, setGlobalTracks] = useRecoilState(GlobalTracks);
+  const submitGlobalTracks = () => {
+    setGlobalTracks(data);
+  };
+  return (
+    <BottomDiv>
+      <OutlineGrid>
+        <OutlineSpan>#</OutlineSpan>
+        <OutlineSpan>제목</OutlineSpan>
+        <OutlineSpan>앨범</OutlineSpan>
+        <OutlineSpan>추가한 날짜</OutlineSpan>
+        <OutlineSpan style={{ justifySelf: 'end' }}>
+          <AccessTimeIcon />
+        </OutlineSpan>
+      </OutlineGrid>
+      <FlexDiv>
+        {data.map((value, index) => (
+          <div onClick={submitGlobalTracks} key={index}>
+            <Song data={value} index={index} />
+          </div>
+        ))}
+      </FlexDiv>
+    </BottomDiv>
+  );
+};
+
+export default LikeBottomDiv;
--- src/pages/WebPlayerTracks.tsx
+++ src/pages/WebPlayerTracks.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import PlaylistMidDiv from '../components/molecules/PlaylistMidDiv';
 import PlaylistBottomDiv from '../components/molecules/PlaylistBottomDiv';
 import { TopDivData, Tracks } from '../stores/SampleData';
 import LikeTopDiv from '../components/molecules/LikeTopDiv';
+import { likeListApi } from '../remote.tsx/collection';
+import LikeBottomDiv from '../components/molecules/LikeBottomDiv';
 
 export const Section = styled.section`
   display: flex;
@@ -15,12 +17,44 @@ export const Section = styled.section`
   margin-right: -24px;
 `;
 
+interface Data {
+  userName: string;
+  musicList: Array<Music>;
+}
+
+interface Music {
+  trackId: string;
+  title: string;
+  seconds: number;
+  singerList: Array<Singer>;
+}
+
+interface Singer {
+  singerId: string;
+  singerName: string;
+  imgUrl: string;
+}
+
 const WebPlayerTracks = () => {
+  const [likeData, setLikeData] = useState<Data | null>();
+  const [len, setLen] = useState<number>(0);
+  useEffect(() => {
+    console.log(likeData);
+    console.log(len);
+  }, [likeData]);
+
+  useLayoutEffect(() => {
+    likeListApi().then((res) => {
+      setLikeData(res.data);
+      setLen(res.data.musicList.length);
+    });
+  }, []);
+
   return (
     <Section>
-      <LikeTopDiv data={TopDivData} num={Tracks.length} />
+      <LikeTopDiv num={len} />
       <PlaylistMidDiv data={Tracks} type={'liked'} />
-      <PlaylistBottomDiv data={Tracks} />
+      <LikeBottomDiv data={Tracks} />
     </Section>
   );
 };
